abde77f02b423baa0bef2567a7062f195957c5f0
Thibault 'Stael' RAMIRES <thibault.ramires@gmail.com>
2014-03-16 00:00:40 +0100

    changement positionnement

--- dock.rb
+++ dock.rb
@@ -8,7 +8,7 @@ load 'point.rb'
 json = JSON.parse(ARGV.join(' '))
 
 if json['cmd'] == 'init'
-  puts "{\"2\":{\"point\":\"22\",\"orientation\":\"vertical\"},\"3\":{\"point\":\"54\",\"orientation\":\"vertical\"},\"4\":{\"point\":\"47\",\"orientation\":\"horizontal\"},\"5\":{\"point\":\"00\",\"orientation\":\"horizontal\"}}"
+  puts "{\"2\":{\"point\":\"22\",\"orientation\":\"vertical\"},\"3\":{\"point\":\"53\",\"orientation\":\"vertical\"},\"4\":{\"point\":\"47\",\"orientation\":\"horizontal\"},\"5\":{\"point\":\"00\",\"orientation\":\"horizontal\"}}"
 else
   battleship = Battleship.new(json)
   battleship.shoot
